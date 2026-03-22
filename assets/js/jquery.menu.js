d491a2876d4209beb74fb33c409a2a4cd36d1a2d
Tom Peer <tom@clik.com>
2026-03-22 06:35:14 +0000

    Rewrite jquery.menu plugin for animated inline and adjacent submenus

--- assets/js/jquery.menu.js
+++ assets/js/jquery.menu.js
@@ -1,149 +1,253 @@
 /**
- * # Menu function
+ * jQuery menu plugin.
  *
- * Add arrow icon to any items with a sub menu
- *
- * Ad onclick to those items that adds an open class to the sub menu.
- *
- * TODO:
- *
- * 1. Auto animate height of sub menus
- * 2. Position sub menus for any that aren't inline
- * 
+ * Features:
+ * - Adds an indicator icon to menu items that contain a `.submenu`.
+ * - Opens/closes sub menus with animation.
+ * - Works for horizontal or vertical menus.
+ * - Supports inline (`--menu-submenu-position:relative`) and adjacent
+ *   (`--menu-submenu-position:absolute`) submenu styles.
  */
-
 (function($) {
+	"use strict";
 
 	$.menu = function(element, options) {
-
 		var defaults = {
 			debug: false,
 			hilight: "",
 			arrow: "<i class='icon-next openicon'></i>",
-			animate: "none", // animate "width", "height" or "both". Use when these CSS props are "auto" and you can't use CSS animations
-			menuAnimationTime: "0.3s",
+			menuAnimationTime: 250,
 			onOpen: function() {},
 			onClose: function() {}
-		}
+		};
 
 		const settingTypes = {
 			hilight: "string",
 			arrow: "string",
-			animate: "string",
 			menuAnimationTime: "string"
 		};
 
 		var plugin = this;
+		var $element = $(element);
+		plugin.settings = {};
+		plugin.mode = {
+			orientation: "horizontal",
+			submenuPosition: "absolute",
+			submenuShow: "hide"
+		};
 
-		plugin.settings = {}
-
-		var $element = $(element), 
-			element = element,
-			props = {};
-
-		plugin.init = function() {
-
-			// the plugin's final properties are the merged default and
-			// user-provided options (if any)
-			let cssSettings = (typeof clik !== "undefined" && typeof clik.parseCssVars === "function") ? (clik.parseCssVars($element, settingTypes) ) : {}; 
-			
-			plugin.settings = $.extend({}, defaults, cssSettings , options);
-			
-			switch(plugin.settings.animate) {
-				case "height":
-					props.height = 0;
-					break;
-				case "width":
-					props.width = 0;
-					break;
-				case "both":
-					props.width = 0;
-					props.height = 0;
-					break;
-			}
-			// code goes here
-			$element.find(".submenu").each(function() {
-				let $submenu = $(this);
-				$submenu.prev("a").append( plugin.settings.arrow ).addClass("hasmenu");
-				$submenu.on("open",function(e) {
-					e.stopPropagation();
-					$submenu.addClass("open").animateAuto(plugin.settings.animate, plugin.settings.menuAnimationTime,function() {
-						$submenu.css({"height":"auto"});
-					});
-				}).on("close",function(e) {
-					e.stopPropagation();
-					console.log(props);
-					$submenu.removeClass("open").animate(props, plugin.settings.menuAnimationTime, function() {});
-				});
-			});
-
-			$element.on("click",".hasmenu .openicon",function(e) {
-				
-				let $item = $(this);
-				e.preventDefault();
-				e.stopPropagation(); 
-				
-				var $li = $item.closest("li");
-				var open = $li.hasClass("open");
-				
-				// close siblings
-				$li.closest("ul").find("li").each(function() {
-					if ($(this).hasClass("open")) {
-						let $submenu = $(this).find("> ul").first();
-						$submenu.trigger("close");
-						$(this).removeClass("open");
-					}
-				});
-				
-				var $submenu = $li.find("> ul").first();
-
-				if (!open) {
-					$li.addClass("open");
-					$submenu.trigger("open");
-				}
-				else {
-					$submenu.trigger("close");	
-				}
-				
-				return false;
-
-			});
-
-			if (plugin.settings.hilight !== "") {
-				let $a = $("#menu_" + plugin.settings.hilight);
-				let $li = $a.closest("li");
-				let $menu = $a.closest("ul");
-				$li.addClass("hi");
-				if ($a.hasClass("hasmenu") ) {
-					let $submenu = $a.find("> ul").first();
-					$li.addClass("open");
-					$submenu.trigger("open");
-				}
-				if ($menu.hasClass("submenu")) {
-					let $parent = $menu.closest("li");
-					$parent.addClass("open");
-				}
+		function debugLog() {
+			if (plugin.settings.debug) {
+				console.log.apply(console, ["[menu]"].concat(Array.prototype.slice.call(arguments)));
 			}
 		}
 
+		function parseDuration(value) {
+			if (typeof value === "number") {
+				return value;
+			}
+			if (typeof value !== "string") {
+				return defaults.menuAnimationTime;
+			}
+			let raw = value.trim();
+			if (raw.endsWith("ms")) {
+				return parseFloat(raw);
+			}
+			if (raw.endsWith("s")) {
+				return parseFloat(raw) * 1000;
+			}
+			let parsed = parseFloat(raw);
+			return Number.isFinite(parsed) ? parsed : defaults.menuAnimationTime;
+		}
+
+		function cssVar(name, fallback) {
+			var value = getComputedStyle($element.get(0)).getPropertyValue(name);
+			if (!value) {
+				return fallback;
+			}
+			return value.trim() || fallback;
+		}
+
+		function readMode() {
+			plugin.mode.orientation = cssVar("--menu-orientation", "horizontal");
+			plugin.mode.submenuPosition = cssVar("--menu-submenu-position", "absolute");
+			plugin.mode.submenuShow = cssVar("--menu-submenu-show", "hide");
+			debugLog("mode", plugin.mode);
+		}
+
+		function isInline() {
+			return plugin.mode.submenuPosition === "relative";
+		}
+
+		function isInlineHorizontal() {
+			return isInline() && plugin.mode.orientation === "horizontal";
+		}
+
+		function closeNode($li, runCallbacks) {
+			if (!$li.length) {
+				return;
+			}
+
+			$li.removeClass("open");
+			var $submenu = $li.children(".submenu").first();
+			$submenu.stop(true, true).attr("aria-hidden", "true").slideUp(plugin.settings.menuAnimationTime, function() {
+				$submenu.removeClass("show");
+				if (runCallbacks) {
+					plugin.settings.onClose.call($submenu.get(0), $li.get(0));
+				}
+			});
+
+			$submenu.find("li.open").each(function() {
+				closeNode($(this), false);
+			});
+		}
+
+		function positionInlineHorizontalSubmenu($li) {
+			if (!isInlineHorizontal()) {
+				return;
+			}
+
+			var $submenu = $li.children(".submenu").first();
+			if (!$submenu.length) {
+				return;
+			}
+
+			var liLeft = $li.position().left;
+			$submenu.css({
+				left: (liLeft * -1) + "px",
+				top: "100%",
+				width: $element.innerWidth() + "px"
+			});
+		}
+
+		function openNode($li) {
+			if (!$li.length) {
+				return;
+			}
+
+			if (!isInline()) {
+				$li.siblings(".open").each(function() {
+					closeNode($(this), true);
+				});
+			}
+
+			var $submenu = $li.children(".submenu").first();
+			if (!$submenu.length) {
+				return;
+			}
+
+			positionInlineHorizontalSubmenu($li);
+			$li.addClass("open");
+			$submenu
+				.addClass("show")
+				.attr("aria-hidden", "false")
+				.stop(true, true)
+				.hide()
+				.slideDown(plugin.settings.menuAnimationTime, function() {
+					$submenu.css("height", "");
+					plugin.settings.onOpen.call($submenu.get(0), $li.get(0));
+				});
+		}
+
+		function decorateMenu() {
+			$element.find(".submenu").each(function() {
+				var $submenu = $(this);
+				var $parentAnchor = $submenu.prev("a");
+				if (!$parentAnchor.length) {
+					return;
+				}
+
+				if (!$parentAnchor.find("> .openicon").length) {
+					$parentAnchor.append(plugin.settings.arrow);
+				}
+
+				$parentAnchor.addClass("hasmenu").attr("aria-haspopup", "true").attr("aria-expanded", "false");
+				$submenu.attr("aria-hidden", "true");
+			});
+		}
+
+		function applyHighlight() {
+			if (plugin.settings.hilight === "") {
+				return;
+			}
+			var $a = $("#menu_" + plugin.settings.hilight);
+			if (!$a.length) {
+				return;
+			}
+			$a.closest("li").addClass("hi");
+			$a.parents("li").each(function() {
+				openNode($(this));
+			});
+		}
+
+		function syncModeClasses() {
+			$element.toggleClass("menu-inline", isInline());
+			$element.toggleClass("menu-adjacent", !isInline());
+			$element.toggleClass("menu-inline-horizontal", isInlineHorizontal());
+		}
+
+		function bindEvents() {
+			$element.off("click.menu");
+			$(window).off("resize.menu");
+
+			if (plugin.mode.submenuShow === "hover") {
+				return;
+			}
+
+			$element.on("click.menu", "a.hasmenu > .openicon", function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+
+				readMode();
+				syncModeClasses();
+
+				var $li = $(this).closest("li");
+				if ($li.hasClass("open")) {
+					closeNode($li, true);
+					$li.children("a").attr("aria-expanded", "false");
+				} else {
+					openNode($li);
+					$li.children("a").attr("aria-expanded", "true");
+				}
+			});
+
+			$(window).on("resize.menu", function() {
+				readMode();
+				syncModeClasses();
+				if (!isInlineHorizontal()) {
+					$element.find(".submenu").css({ left: "", top: "", width: "" });
+					return;
+				}
+				$element.find("li.open").each(function() {
+					positionInlineHorizontalSubmenu($(this));
+				});
+			});
+		}
+
+		plugin.init = function() {
+			let cssSettings = (typeof clik !== "undefined" && typeof clik.parseCssVars === "function")
+				? clik.parseCssVars($element, settingTypes)
+				: {};
+
+			plugin.settings = $.extend({}, defaults, cssSettings, options);
+			plugin.settings.menuAnimationTime = parseDuration(plugin.settings.menuAnimationTime);
+
+			readMode();
+			decorateMenu();
+			syncModeClasses();
+			bindEvents();
+			applyHighlight();
+		};
+
 		plugin.init();
-		
-	}
+	};
 
 	$.fn.menu = function(options) {
-
 		return this.each(function() {
-
-		  if (undefined == $(this).data('menu')) {
-
-			  var plugin = new $.menu(this, options);
-
-			  $(this).data('menu', plugin);
-
-		   }
-
+			if (undefined === $(this).data("menu")) {
+				var plugin = new $.menu(this, options);
+				$(this).data("menu", plugin);
+			}
 		});
-
-	}
-
+	};
 })(jQuery);
